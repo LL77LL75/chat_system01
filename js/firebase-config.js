72a2123e13f6fd8aca726811e2e5a731afa1e7fe
LL77LL75 <lancelimenze13@gmail.com>
2025-11-18 08:11:45 +0000

    Nov 18, 2025, 8:11 AM

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,19 +1,19 @@
 // firebase-config.js
-// Converted from your <script type="module"> snippet
+// Clean version of your script tag rewritten as a JS module file.
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-analytics.js";
 
 // Your Firebase configuration
-export const firebaseConfig = {
-  apiKey: "AIzaSyCY5_krGDfHcp4ZmUe5RXo7BaKYUQwAM8E",
-  authDomain: "chat-app-6767.firebaseapp.com",
-  databaseURL: "https://chat-app-6767-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "chat-app-6767",
-  storageBucket: "chat-app-6767.firebasestorage.app",
-  messagingSenderId: "705833150639",
-  appId: "1:705833150639:web:618339099f129a4ccacc5a",
-  measurementId: "G-3P9WH9S08E"
+const firebaseConfig = {
+    apiKey: "AIzaSyCY5_krGDfHcp4ZmUe5RXo7BaKYUQwAM8E",
+    authDomain: "chat-app-6767.firebaseapp.com",
+    databaseURL: "https://chat-app-6767-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "chat-app-6767",
+    storageBucket: "chat-app-6767.firebasestorage.app",
+    messagingSenderId: "705833150639",
+    appId: "1:705833150639:web:618339099f129a4ccacc5a",
+    measurementId: "G-3P9WH9S08E"
 };
 
 // Initialize Firebase
