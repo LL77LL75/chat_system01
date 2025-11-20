50c51214397bc984d1589d1ca09477df56ca4bc6
LL77LL75 <lancelimenze13@gmail.com>
2025-11-20 11:39:58 +0000

    Nov 20, 2025, 11:39 AM

--- firebase-config.js
+++ firebase-config.js
@@ -1,19 +1,19 @@
-// Import Firebase functions
+// firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCY5_krGDfHcp4ZmUe5RXo7BaKYUQwAM8E",
-  authDomain: "chat-app-6767.firebaseapp.com",
-  databaseURL: "https://chat-app-6767-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "chat-app-6767",
-  storageBucket: "chat-app-6767.firebasestorage.app",
-  messagingSenderId: "705833150639",
-  appId: "1:705833150639:web:618339099f129a4ccacc5a",
-  measurementId: "G-3P9WH9S08E"
+    apiKey: "AIzaSyCY5_krGDfHcp4ZmUe5RXo7BaKYUQwAM8E",
+    authDomain: "chat-app-6767.firebaseapp.com",
+    databaseURL: "https://chat-app-6767-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "chat-app-6767",
+    storageBucket: "chat-app-6767.firebasestorage.app",
+    messagingSenderId: "705833150639",
+    appId: "1:705833150639:web:618339099f129a4ccacc5a",
+    measurementId: "G-3P9WH9S08E"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getDatabase(app);  // export db for app.js usage
+export const db = getDatabase(app);
