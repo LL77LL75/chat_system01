797bc4a326664bfc91a79be5dc641a4b461bd735
LL77LL75 <lancelimenze13@gmail.com>
2025-11-21 08:52:50 +0000

    Nov 21, 2025, 8:52 AM

---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-database.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCY5_krGDfHcp4ZmUe5RXo7BaKYUQwAM8E",
+    authDomain: "chat-app-6767.firebaseapp.com",
+    databaseURL: "https://chat-app-6767-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "chat-app-6767",
+    storageBucket: "chat-app-6767.firebasestorage.app",
+    messagingSenderId: "705833150639",
+    appId: "1:705833150639:web:618339099f129a4ccacc5a",
+    measurementId: "G-3P9WH9S08E"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
